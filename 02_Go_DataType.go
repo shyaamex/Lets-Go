f8541cc790244607bf8f8091d6df174cf99a7834
shyaamex <shyaam.ex@gmail.com>
2023-03-20 21:44:17 +0530

    Second file added

---
+++ 02_Go_DataType.go
@@ -0,0 +1,19 @@
+/*
+	Go Datatype & Variable
+
+
+Go is statically typed, meaning that ONCE a variable type is DEFINED, it can ONLY store data of that type.
+
+
+Go has mainly three Datatypes:
+- String
+- Numeric - Float & int
+- Boolean
+
+
+
+
+*/
+
+
+
