83ac1d6624398c8779baed837d9a5e916fd8aee9
Shyaam <115785301+shyaamex@users.noreply.github.com>
2023-03-22 23:09:23 +0530

    Update 02_Go_DataType.go

--- 02_Go_DataType.go
+++ 02_Go_DataType.go
@@ -19,8 +19,8 @@ package main
 import ("fmt")
 
 func main() {
-  var a bool = true     // Boolean
-  var b int = 5         // Integer
+  var a bool = true     // Numeric-  Boolean
+  var b int = 5         // Numeric-  Integer
   var c float32 = 3.14  // Floating point number
   var d string = "Hi!"  // String
 
